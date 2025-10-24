f2e921e9684de31f4d8cf10dbd785ad0c3e854c9
서창열 <kongbaguni@gmail.com>
2025-10-24 10:22:50 +0900

    이미지 버튼 스타일 정의 추가

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # KongUIKit 
+
+
+## KImageButton 
+
+```Swift
+        KImageButton(image: .init(systemName: "circle"),
+                     title: .init("test test"),
+                     style: .init(foregroundColor: .yellow, backgroundColor: .black, padding: 10, cornerRadius: 30)
+        ) {
+            print("test test")
+        }
+        KImageButton(image: .init(systemName: "circle")
+        ) {
+            print("test test")
+        }
+```
+
--- Sources/KongUIKit/KImageButton.swift
+++ Sources/KongUIKit/KImageButton.swift
@@ -8,27 +8,69 @@
 import SwiftUI
 
 public struct KImageButton: View {
+    
+    public struct Style {
+        let foregroundColor: Color
+        let backgroundColor: Color
+        let padding:CGFloat
+        let cornerRadius:CGFloat
+        public init(foregroundColor: Color, backgroundColor: Color, padding: CGFloat, cornerRadius: CGFloat) {
+            self.foregroundColor = foregroundColor
+            self.backgroundColor = backgroundColor
+            self.padding = padding
+            self.cornerRadius = cornerRadius
+        }
+    }
+    
     let image: Image
     let onClick: () -> Void
+    let title:Text?
+    let style:Style
     
-    public init(image: Image, onClick: @escaping () -> Void) {
+    public init(image: Image,
+                title:Text? = nil,
+                style:Style = .init(foregroundColor: .primary, backgroundColor: .secondary, padding: 5, cornerRadius: 10),
+                onClick: @escaping () -> Void) {
         self.image = image
         self.onClick = onClick
-    }
+        self.title = title
+        self.style = style
+  }
     
     public var body: some View {
         Button {
             onClick()
         } label: {
-            image.resizable()
-                .scaledToFit()
+            VStack {
+                image.resizable()
+                    .renderingMode(.original)
+                    .scaledToFit()
+                    .foregroundStyle(style.foregroundColor)
+                if let title {
+                    title.foregroundColor(style.foregroundColor)
+                }
+            }
+            .padding(style.padding)
+            .background {
+                RoundedRectangle(cornerRadius: style.cornerRadius)
+                    .fill(style.backgroundColor)
+            }
         }
     }
 }
 
 #Preview {
-    KImageButton(image: .init(systemName: "circle")) {
-        print("test")
-    }
-    .frame(width: 30, height: 30)
+    HStack (spacing:5) {
+        KImageButton(image: .init(systemName: "circle"),
+                     title: .init("test test"),
+                     style: .init(foregroundColor: .yellow, backgroundColor: .black, padding: 10, cornerRadius: 30)
+        ) {
+            print("test test")
+        }
+        KImageButton(image: .init(systemName: "circle")
+        ) {
+            print("test test")
+        }
+    }.padding(5)
+    
 }
