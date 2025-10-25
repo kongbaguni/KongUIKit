d6943db3f0328fd3d8947fc451d2ad1e1541d03e
서창열 <kongbaguni@gmail.com>
2025-10-25 16:10:30 +0900

    코너 둥글게 깎기

--- Sources/KongUIKit/KBarcodeView.swift
+++ Sources/KongUIKit/KBarcodeView.swift
@@ -16,6 +16,7 @@ fileprivate extension String {
         
         guard let data = self.data(using: .ascii) else { return nil }
         filter.message = data
+        
         guard let outputImage = filter.outputImage else { return nil }
         
         // 바코드를 크게 확대해서 선명하게 표시
@@ -29,12 +30,14 @@ fileprivate extension String {
 }
 
 public struct KBarcodeView: View {
-    public init(text: String) {
+    public init(text: String, conerRadius: CGFloat = 0) {
         self.text = text
+        self.cornerRadius = conerRadius
     }
     
     let text:String
-
+    let cornerRadius:CGFloat
+    
     public var body: some View {
         VStack {
             if let image = text.barcodeImage {
@@ -42,6 +45,7 @@ public struct KBarcodeView: View {
                     .resizable()
                     .interpolation(.none) // 픽셀 보존
                     .scaledToFit()
+                    .cornerRadius(cornerRadius)
             } else {
                 Text("바코드를 생성할 수 없습니다.")
                     .foregroundColor(.secondary)
@@ -51,5 +55,8 @@ public struct KBarcodeView: View {
     }
 }
 #Preview {
-    KBarcodeView(text: "1234").frame(height: 100)
+    VStack {
+        KBarcodeView(text: "12341").frame(height: 100)
+            .padding(20)
+    }.background(.orange)
 }
