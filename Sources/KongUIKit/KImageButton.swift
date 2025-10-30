1fd6fdc093c15b133a1cf23f235aa958f4dd1a34
서창열 <kongbaguni@gmail.com>
2025-10-30 23:04:32 +0900

    ImageButton 수정

--- Sources/KongUIKit/KImageButton.swift
+++ Sources/KongUIKit/KImageButton.swift
@@ -9,19 +9,37 @@ import SwiftUI
 
 public struct KImageButton: View {
     
+    
     public struct Style : Sendable {
         let foregroundColor: Color
         let backgroundColor: Color
         let padding:CGFloat
         let cornerRadius:CGFloat
-        public init(foregroundColor: Color, backgroundColor: Color, padding: CGFloat, cornerRadius: CGFloat) {
+        let isHorizontal:Bool
+        
+        public init(
+            foregroundColor: Color,
+            backgroundColor: Color,
+            padding: CGFloat,
+            cornerRadius: CGFloat,
+            isHorizontal: Bool = true
+        ) {
             self.foregroundColor = foregroundColor
             self.backgroundColor = backgroundColor
             self.padding = padding
             self.cornerRadius = cornerRadius
+            self.isHorizontal = isHorizontal
         }
         
         public static let simple: Style = .init(foregroundColor: .primary, backgroundColor: .clear, padding: 5, cornerRadius: 10)
+        
+        public static let horizontalButton : Style = .init(
+            foregroundColor: .primary,
+            backgroundColor: .teal,
+            padding: 10,
+            cornerRadius: 20,
+            isHorizontal: true
+        )
     }
     
     let image: Image
@@ -37,19 +55,34 @@ public struct KImageButton: View {
         self.onClick = onClick
         self.title = title
         self.style = style
-  }
+    }
     
     public var body: some View {
         Button {
             onClick()
         } label: {
-            VStack {
-                image.resizable()
-                    .renderingMode(.original)
-                    .scaledToFit()
-                    .foregroundStyle(style.foregroundColor)
-                if let title {
-                    title.foregroundColor(style.foregroundColor)
+            Group {
+                if style.isHorizontal {
+                    HStack {
+                        image.resizable()
+                            .renderingMode(.original)
+                            .scaledToFit()
+                            .foregroundStyle(style.foregroundColor)
+                        if let title {
+                            title.foregroundColor(style.foregroundColor)
+                        }
+                    }
+                }
+                else {
+                    VStack {
+                        image.resizable()
+                            .renderingMode(.original)
+                            .scaledToFit()
+                            .foregroundStyle(style.foregroundColor)
+                        if let title {
+                            title.foregroundColor(style.foregroundColor)
+                        }
+                    }
                 }
             }
             .padding(style.padding)
@@ -57,23 +90,39 @@ public struct KImageButton: View {
                 RoundedRectangle(cornerRadius: style.cornerRadius)
                     .fill(style.backgroundColor)
             }
+            
         }
     }
 }
 
 #Preview {
-    HStack (spacing:5) {
+    VStack {
+        HStack (spacing:5) {
+            KImageButton(image: .init(systemName: "circle"),
+                         title: .init("test test"),
+                         style: .init(foregroundColor: .yellow,
+                                      backgroundColor: .black,
+                                      padding: 10,
+                                      cornerRadius: 30,
+                                      isHorizontal: false)
+            ) {
+                print("test test")
+            }
+            KImageButton(image: .init(systemName: "circle"),
+                         style: .simple
+            ) {
+                print("test test")
+            }
+        }.padding(5)
+        
         KImageButton(image: .init(systemName: "circle"),
-                     title: .init("test test"),
-                     style: .init(foregroundColor: .yellow, backgroundColor: .black, padding: 10, cornerRadius: 30)
+                     title: .init("asdasd asd"),
+                     style: .horizontalButton
         ) {
             print("test test")
-        }
-        KImageButton(image: .init(systemName: "circle"),
-                     style: .simple
-        ) {
-            print("test test")
-        }
-    }.padding(5)
+        }.frame(height: 50)
+        
+    }
+    
     
 }
